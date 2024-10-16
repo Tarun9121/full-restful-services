87cef7830122a83a4995fe127adf209e9025e127
Tarun9121 <tarunswaroop199@gmail.com>
2024-10-16 15:32:57 +0530

    unidirectional

--- src/main/java/com/restful/entity/Address.java
+++ src/main/java/com/restful/entity/Address.java
@@ -11,7 +11,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.UUID;
 
@@ -29,10 +28,6 @@ public class Address {
 
     @Column(name = "city")
     private String city;
-
-//    @OneToOne(mappedBy = "address")
-//    @JsonIgnore
-//    private Student student;
 
     @Override
     public String toString() {
